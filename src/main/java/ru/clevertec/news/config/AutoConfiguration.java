bfe3f83c1ed4269970b01e011a1d33296e7222ee
vitmvit <120332119+vitmvit@users.noreply.github.com>
2024-03-04 16:44:50 +0300

    refactor

--- src/main/java/ru/clevertec/news/config/AutoConfiguration.java
+++ src/main/java/ru/clevertec/news/config/AutoConfiguration.java
@@ -1,5 +1,8 @@
 package ru.clevertec.news.config;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -8,14 +11,29 @@ import ru.clevertec.news.handler.GlobalExceptionHandler;
 
 /**
  * Автоконфигурация для обработки исключений.
- * Конфигурация может быть включена и выключена с помощью свойства "handler.enabled" в файле application.yml.
+ * Конфигурация может быть включена и выключена с помощью свойства "starter.handler.enabled" в файле application.yml.
  */
+@Slf4j
 @Configuration
 @EnableConfigurationProperties
-@ConditionalOnProperty(prefix = "handler", name = "enabled", havingValue = "true")
+@ConditionalOnProperty(prefix = "starter.handler", name = "enabled", havingValue = "true", matchIfMissing = true)
 public class AutoConfiguration {
 
+    /**
+     * Инициализирует класс AutoConfiguration и регистрирует сообщение инициализации
+     */
+    @PostConstruct
+    void init() {
+        log.info("AutoConfiguration initialized");
+    }
+
+    /**
+     * Возвращает bean-компонент GlobalExceptionHandler
+     *
+     * @return GlobalExceptionHandler bean.
+     */
     @Bean
+    @ConditionalOnMissingBean
     public GlobalExceptionHandler create() {
         return new GlobalExceptionHandler();
     }
